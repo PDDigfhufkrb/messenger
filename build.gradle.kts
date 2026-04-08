66c0711792159b64f8c7290e5fc7b093e4dccbf6
PDD-shpargalki <eksort.aurel@gmail.com>
2026-04-09 00:03:58 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// build.gradle.kts (Project)
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}

619f593b01cb18853e23c3b72c417ccf3bb11ff0
PDD-shpargalki <eksort.aurel@gmail.com>
2026-04-09 23:56:45 +0300

    Update AppModule.kt

--- di/AppModule.kt
+++ di/AppModule.kt
@@ -4,16 +4,14 @@ import com.hemax.database.AppDatabase
 import com.hemax.repositories.*
 import com.hemax.tdlib.TdConfig
 import com.hemax.tdlib.TdLibClient
-import com.hemax.viewmodels.*
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    single { TdConfig(apiId = YOUR_API_ID, apiHash = "YOUR_API_HASH") }
+    single { TdConfig(apiId = 123456, apiHash = "YOUR_API_HASH") } // Замените на реальные
     single { TdLibClient(androidContext(), get()) }
 
-    single { AppDatabase.getInstance(androidContext(), "your_strong_password") }
+    single { AppDatabase.getInstance(androidContext(), "strong_password") }
     single { get<AppDatabase>().userDao() }
     single { get<AppDatabase>().chatDao() }
     single { get<AppDatabase>().messageDao() }
@@ -22,11 +20,12 @@ val appModule = module {
     single { get<AppDatabase>().scheduledMessageDao() }
 
     single<AuthRepository> { AuthRepositoryImpl(get(), get()) }
-    // single<ChatRepository> { ChatRepositoryImpl(get(), get()) }
-    // single<MessageRepository> { MessageRepositoryImpl(get(), get()) }
-    // ... остальные репозитории
-
-    viewModelOf(::AuthViewModel)
-    // viewModelOf(::ChatsViewModel)
-    // viewModelOf(::ChatViewModel)
+    single<ChatRepository> { ChatRepositoryImpl(get(), get()) }
+    single<MessageRepository> { MessageRepositoryImpl(get()) }
+    // Остальные репозитории пока заглушки, добавьте по мере необходимости
+    single<UserRepository> { TODO() }
+    single<SettingsRepository> { TODO() }
+    single<GroupRepository> { TODO() }
+    single<ScheduledMessageRepository> { TODO() }
+    single<NotificationRepository> { TODO() }
 }
